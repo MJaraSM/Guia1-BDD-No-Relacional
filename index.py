d119a373bee9a87a16faa25703e56e4ac4985ba8
MJaraSM <145721388+MJaraSM@users.noreply.github.com>
2024-04-06 16:42:02 -0300

    Agregamos un menu

--- index.py
+++ index.py
@@ -12,42 +12,41 @@ collection="Pokemon"
 cliente = pymongo.MongoClient(URL, serverSelectionTimeoutMS=TIME_OUT)
 BaseDatos = cliente[database]
 Collection = BaseDatos[collection]
+seleccion = 0
 
 def VerDatos():
     try:
-        cliente.server_info()
-        print("Conexion Exitosa")
         for pokemones in Collection.find():
             print(pokemones)
-        print(" ")
-        for pokemones in Collection.find():
-            print(str(pokemones["Numero"])+" "+pokemones["Nombre"]+" "+pokemones["Tipo1"]+" "+pokemones["Tipo2"])
-        cliente.close()
-    except pymongo.errors.serverSelectionTimeoutError as errorTiempo:
-        print("Tiempo exedido "+ errorTiempo)
-    except pymongo.errors.ConnectionFailure as errorConexion:
-        print("Error de conexion"+errorConexion)
+    except:
+        print("A ocurrido un error")
 
 def InsertarDatos():
-    Numero=1
-    Nombre="Bulbasaur"
-    Tipo1="Planta"
-    Tipo2="Ninguno"
+    print("Vamos a añadir un nuevo pokemon")
+    Numero = input("Primero, ingrese el numero de pokedex: ")
+    Nombre = input("Ahora, ingrese el nombre del pokemon: ")
+    Tipo1 = input("Ingrese el primer tipo del pokemon: ")
+    Tipo2 = input("Para terminar, ingrese el segundo tipo, si no tiene, escriba ninguno: ")
     try:
         pokemon={"Numero":Numero,
                 "Nombre":Nombre,
                 "Tipo1":Tipo1,
                 "Tipo2":Tipo2}
         Collection.insert_one(pokemon)
+        print
     except pymongo.errors.ConnectionFailure as error:
         print(error)
 
 def ModificarDatos():
-    #Recordatorio = Mantener la id actualizada si lo borra mas adelante
-    ID = "660e156255119eefb485ab00"
-    pokemon= Collection.find({"_id": ObjectId(ID)})[0]
+    Valido = 0
+    while Valido == 0:
+        ID = input("Ingrese el ID del Pokemon a eliminar: ")
+        try:
+            pokemon= Collection.find({"_id": ObjectId(ID)})[0]
+            break
+        except:
+            print("El id ingresado no es valido")
     print(pokemon)
-    #Seleccione los datos a cambiar
     nuevos_datos = {
             "Numero": 9,
             "Nombre": "Blastoise",
@@ -58,16 +57,19 @@ def ModificarDatos():
     print(pokemon)
 
 def EliminarDatos():
-    ID = "660e14fb39d5807fddffcf38"
-    # Buscar el Pokémon por su ID
-    pokemon = Collection.find_one({"_id": ObjectId(ID)})
+    Valido = 0
+    while Valido == 0:
+        ID = input("Ingrese el ID del Pokemon a eliminar: ")
+        try:
+            pokemon = Collection.find_one({"_id": ObjectId(ID)})
+            break
+        except:
+            print("El id ingresado no es valido")
     if pokemon:
         print("Pokemon encontrado:")
         print(pokemon)
-        # Confirmar si se desea eliminar
         confirmacion = input("¿Está seguro de que desea eliminar este Pokémon? (s/n): ")
         if confirmacion.lower() == "s":
-            # Eliminar el documento de la colección
             Collection.delete_one({"_id": ObjectId(ID)})
             print("Pokemon eliminado exitosamente.")
         else:
@@ -75,8 +77,35 @@ def EliminarDatos():
     else:
         print("Pokemon no encontrado.")
 
-#Seleccione la parte del crud que quiere probar
-#VerDatos()
-#InsertarDatos()
-#ModificarDatos()
-#EliminarDatos()
+try:
+    cliente.server_info()
+    print("Conexion a la BDD Exitosa")
+    while(seleccion ==0):
+        print("Seleccione la acción a realizar: ")
+        print(" ")
+        print("1. Ver")
+        print("2. Insertar")
+        print("3. Modificar")
+        print("4. Eliminar")
+        respuesta = input("Respuesta: ")
+        if respuesta == "1":
+            VerDatos()
+        if respuesta == "2":  
+            InsertarDatos()
+        if respuesta == "3":
+            ModificarDatos()
+        if respuesta == "4":
+            EliminarDatos()
+        print(" ")
+        print("¿Desea volver al menu inicial?")
+        print("1. Si "+"    "+" 2. No")
+        respuesta2 = input("Respuesta: ")
+        print(" ")
+        if respuesta2 == "2":
+            break
+    cliente.close()
+except pymongo.errors.serverSelectionTimeoutError as errorTiempo:
+    print("Tiempo exedido "+ errorTiempo)
+except pymongo.errors.ConnectionFailure as errorConexion:
+    print("Error de conexion"+errorConexion)
+
